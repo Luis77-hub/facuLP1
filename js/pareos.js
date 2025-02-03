0900c188e2f8355faa444b6cc24cd16002a43dde
Luis77-hub <lf9567945@gmail.com>
2025-02-03 19:52:00 -0300

    examen.parciel

---
+++ js/pareos.js
@@ -0,0 +1,15 @@
+//conversion de string a number
+let numerotexto = "48";
+let convertidonumero = Number(numerotexto);
+console.log(convertidonumero, typeof(convertidonumero));
+
+//conversion de string a boolean
+let lazoe = "Zoe";
+let convertida = Boolean(lazoe);
+console.log(lazoe,convertida);
+
+
+function saludo(nombre,apellido,cedula) {
+    console.log("Hola");
+}
+saludo("Moncha Yamile","ladebarriozombie",5687453);
